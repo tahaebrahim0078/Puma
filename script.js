000ba50e5d1affd52ad78c42b49e6e206494a945
unknown <bamrmagdy@gmail.com>
2025-08-03 06:03:52 +0300

    Handeled Color Picker With JS

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 const listItems = document.getElementsByClassName("listItem");
+const ColorImages = document.getElementsByClassName("colorImg");
+
 const handleOnActiveListItem = function (e) {
-  Array.from(listItems).forEach((element) =>
-    element.classList.remove("active")
-  );
+  Array.from(listItems).forEach((element) => {
+    element.classList.remove("active");
+
+    const currentColor = element.id;
+    element.src = `/Assets/${currentColor}.png`; // رجع الصورة العادية
+  });
+
   e.currentTarget.classList.add("active");
 };
+
+function colorImgClickHandler(e) {
+  if (e.target.classList.contains("activeColor")) return;
+
+  Array.from(ColorImages).forEach((element) => {
+    element.classList.remove("activeColor");
+
+    const currentColor = element.id;
+    element.src = `/Assets/color${currentColor}.png`;
+  });
+
+  e.target.classList.add("activeColor");
+  e.target.src = `/Assets/colorActive.png`;
+}
 
 Array.from(listItems).forEach((element) => {
   element.addEventListener("click", handleOnActiveListItem);
 });
-console.log(listItems);
+
+Array.from(ColorImages).forEach((element) => {
+  element.addEventListener("click", colorImgClickHandler);
+});
