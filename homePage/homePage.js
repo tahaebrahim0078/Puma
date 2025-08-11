6f3448be709ee4f0e3b8cbad2c82d132b8ff2c7a
unknown <bamrmagdy@gmail.com>
2025-08-11 16:54:10 +0300

    fix responsive bugs in product page

--- homePage/homePage.js
+++ homePage/homePage.js
@@ -2,7 +2,12 @@ const aboutUsAnchor = document.getElementById("about_Us");
 
 const aboutUsAnchorClickHandler = function () {
   window.open("https://www.linkedin.com/in/amrmagdydb/", "_blank");
-  window.open("https://www.linkedin.com/in/taha-ibrahim-24918b265/", "_blank");
+  setTimeout(() => {
+    window.open(
+      "https://www.linkedin.com/in/taha-ibrahim-24918b265/",
+      "_blank"
+    );
+  }, 500);
 };
 
 aboutUsAnchor.addEventListener("click", aboutUsAnchorClickHandler);
