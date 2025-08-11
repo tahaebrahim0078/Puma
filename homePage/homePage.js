2670496fda2e1588ea6815d7fd65e54f7b09408d
unknown <bamrmagdy@gmail.com>
2025-08-11 17:08:49 +0300

    addLinkedinProfineInAboutUs

--- homePage/homePage.js
+++ homePage/homePage.js
@@ -2,12 +2,7 @@ const aboutUsAnchor = document.getElementById("about_Us");
 
 const aboutUsAnchorClickHandler = function () {
   window.open("https://www.linkedin.com/in/amrmagdydb/", "_blank");
-  setTimeout(() => {
-    window.open(
-      "https://www.linkedin.com/in/taha-ibrahim-24918b265/",
-      "_blank"
-    );
-  }, 500);
+  window.open("https://www.linkedin.com/in/taha-ibrahim-24918b265/");
 };
 
 aboutUsAnchor.addEventListener("click", aboutUsAnchorClickHandler);
