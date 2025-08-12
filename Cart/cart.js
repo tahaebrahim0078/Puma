89afabe0757028fcf22a4995bc999c920cc1bdcd
unknown <bamrmagdy@gmail.com>
2025-08-12 08:13:34 +0300

    Add LocalStorage To cart Page With Functionality

---
+++ Cart/cart.js
@@ -0,0 +1,85 @@
+const BillContainer = document.querySelector(".bill-container");
+const totalPrice = document.querySelector("#total_price");
+const onLoadPageBillContainerHandler = function () {
+  const cartData = getLocalStorageCartData();
+
+  if (cartData) {
+    cartData.forEach((product) => {
+      const productInfoInHTML = `
+       <div class="bill-box" id="${product.productId}">
+                <div class="box-image">
+                  <img src="${product.photoSrc}" alt="Product Image" />
+                </div>
+                <div class="bill-description">
+                  <p>
+                    ${product.productDescription}
+                  </p>
+                </div>
+                <div class="bill-quantity">  <input price =${product.productPrice} id="qty_counter"type="number" value="1" min="1" />
+</div>
+                <div class="bill-price">
+                <span>${product.productPrice}.LE</span></div>
+                <div class="bill-deleteBtn">
+                  <button class="delete_btn"id="${product.productId}">Delete</button>
+                </div>
+              </div>
+    `;
+
+      BillContainer.insertAdjacentHTML("beforeend", productInfoInHTML);
+    });
+  } else {
+    return;
+  }
+  const billTotalPrice = getBillTotalPrice(cartData);
+  totalPrice.textContent = billTotalPrice;
+};
+
+const getLocalStorageCartData = function () {
+  const cartData = window.localStorage.getItem("cart");
+  if (cartData) {
+    return JSON.parse(cartData);
+  }
+
+  return false;
+};
+const getBillTotalPrice = function (products) {
+  console.log(products);
+  return products.reduce((sum, product) => {
+    return (sum += Number(product.productPrice));
+  }, 0);
+};
+
+window.onload = onLoadPageBillContainerHandler;
+
+document.addEventListener("click", (event) => {
+  if (event.target && event.target.matches(".delete_btn")) {
+    handleDeleteBtnClick(Number(event.target.id));
+  } else if (event.target && event.target.matches("#qty_counter")) {
+    let currentTotalPrice = Number(totalPrice.textContent);
+    const quantity = Number(event.target.value);
+    const price = Number(event.target.getAttribute("price"));
+
+    let currentValue = event.target.getAttribute("current_value");
+    currentValue = currentValue ? Number(currentValue) : 1;
+
+    if (currentValue > quantity) {
+      currentTotalPrice -= price * (currentValue - quantity);
+    } else if (currentValue < quantity) {
+      currentTotalPrice += price * (quantity - currentValue);
+    }
+    event.target.setAttribute("current_value", quantity);
+
+    totalPrice.textContent = currentTotalPrice;
+  }
+});
+
+const handleDeleteBtnClick = function (productID) {
+  let cartData = getLocalStorageCartData();
+  console.log(cartData);
+  cartData = cartData.filter((product) => {
+    return product.productId !== productID;
+  });
+  console.log(cartData);
+  localStorage.setItem("cart", JSON.stringify(cartData));
+  window.location.reload();
+};
--- Product/product.js
+++ Product/product.js
@@ -1,6 +1,46 @@
 const listItems = document.getElementsByClassName("listItem");
 const ColorImages = document.getElementsByClassName("colorImg");
+const addToCartBtn = document.getElementById("addToCarBtn");
+const productPhoto = document.getElementById("mainChose");
+const productName = document.getElementById("product_name");
+const productDescription = document.getElementById("product_description");
+const productPrice = document.getElementById("product_price");
 
+const handleaddToCartBtn = function () {
+  const photoSRC = productPhoto.getAttribute("src");
+  const name = productName.textContent;
+  const description = productDescription.textContent;
+  const price = productPrice.textContent;
+  const id = getLocalStorageLength() + 1;
+  const productObj = {
+    photoSrc: photoSRC,
+    productName: name,
+    productDescription: description,
+    productPrice: price,
+    productId: id,
+  };
+  addProductToLocalStorage(productObj);
+};
+
+const getLocalStorageLength = function () {
+  const cartData = window.localStorage.getItem("cart");
+  if (cartData) {
+    return JSON.parse(cartData).length;
+  }
+
+  return 0;
+};
+
+const addProductToLocalStorage = function (productObj) {
+  if (window.localStorage.getItem("cart")) {
+    const cartData = JSON.parse(window.localStorage.getItem("cart"));
+    cartData.push(productObj);
+    localStorage.setItem("cart", JSON.stringify(cartData));
+    return;
+  }
+  const cartData = [productObj];
+  localStorage.setItem("cart", JSON.stringify(cartData));
+};
 const handleOnActiveListItem = function (e) {
   Array.from(listItems).forEach((element) => {
     element.classList.remove("active");
@@ -32,3 +72,5 @@ Array.from(listItems).forEach((element) => {
 Array.from(ColorImages).forEach((element) => {
   element.addEventListener("click", colorImgClickHandler);
 });
+
+addToCartBtn.addEventListener("click", handleaddToCartBtn);
